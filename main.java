f3398de4a27007d69db25f32958cdeee7db4b9ea
vaibhavgupta777-coder <vaibhavgupta1893@gmail.com>
2026-03-12 10:21:16 +0530

    created uc 6

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("  ***     ***     *****    ***** ");
+        System.out.println(" **  **   **  **   **  **  **     ");
+        System.out.println("**    ** **    **  **  **  **     ");
+        System.out.println("**    ** **    **  *****    ***** ");
+        System.out.println("**    ** **    **  **            **");
+        System.out.println(" **  **   **  **   **       **   **");
+        System.out.println("  ***     ***     **         ***** ");
+
+    }
+}
